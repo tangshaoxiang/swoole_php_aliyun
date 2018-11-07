a771fe595692e5dc83b78b9020f88efb08fbfe30
darian <873246925@qq.com>
2018-11-08 01:49:54 +0800

    t_up

--- serve/http_server.php
+++ serve/http_server.php
@@ -5,8 +5,14 @@
  * Date: 2018/11/8
  * Time: 0:09
  */
-$http = new swoole_http_server("0.0.0.0",8812);
+$http = new swoole_http_server("0.0.0.0",8811);
+$http->set(
+    [
+        'enable_static_handler'=>true,
+        'document_root'=>"/home/wwwroot/www.darian.xin/swoole_php_aliyun/data"
+    ]
+);
 $http->on('request', function ($request, $response) {
-    $response->end("alis");
+    $response->end("love");
 });
 $http->start();
