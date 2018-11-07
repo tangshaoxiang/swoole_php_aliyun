7bc4624231ed37fce3ed1ea6cd383512d61345d0
darian <873246925@qq.com>
2018-11-08 01:09:31 +0800

    t_up

--- serve/http_server.php
+++ serve/http_server.php
@@ -13,6 +13,6 @@ $http = new swoole_http_server("0.0.0.0",8811);
 //    ]
 //);
 $http->on('request', function ($request, $response) {
-    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
+    $response->end("ssss");
 });
 $http->start();
