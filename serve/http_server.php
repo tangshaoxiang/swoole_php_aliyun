0ed8f2db97b2bb37411894cb2c19972ff82ccadd
darian <873246925@qq.com>
2018-11-08 01:36:23 +0800

    t_up

--- serve/http_server.php
+++ serve/http_server.php
@@ -5,7 +5,7 @@
  * Date: 2018/11/8
  * Time: 0:09
  */
-$http = new swoole_http_server("0.0.0.0",8811);
+$http = new swoole_http_server("0.0.0.0",'8811');
 $http->on('request', function ($request, $response) {
     $response->end("alis");
 });
