8d94bf2560cc78357a71fb31bfc1fa7f8d51d1ff
darian <873246925@qq.com>
2018-11-17 16:44:32 +0800

    t_up

---
+++ io/write.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ty_Ro
+ * Date: 2018/11/17
+ * Time: 16:42
+ */
+$file_content = '佛冷';
+swoole_async_writefile(__DIR__.'/test.log', $file_content, function($filename) {
+    echo $filename."wirte ok.\n";
+}, $flags = 0);
