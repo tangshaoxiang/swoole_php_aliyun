8be46072dfbdffa15ad9e45ac2481f400df1d16b
darian <873246925@qq.com>
2018-11-17 16:47:29 +0800

    t_up

--- io/write.php
+++ io/write.php
@@ -6,6 +6,6 @@
  * Time: 16:42
  */
 $file_content = '佛冷';
-swoole_async_writefile(__DIR__.'/test.log', $file_content, function($filename) {
+swoole_async_writefile(__DIR__.'/test.log', $file_content.PHP_EOL, function($filename) {
     echo $filename."wirte ok.\n";
 }, FILE_APPEND);
