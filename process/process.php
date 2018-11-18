b3974b71583bb54a89c143e7fedfb006173759bd
darian <873246925@qq.com>
2018-11-18 23:02:59 +0800

    t_up

---
+++ process/process.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ty_Ro
+ * Date: 2018/11/18
+ * Time: 22:51
+ */
+$process = new swoole_process(function (swoole_process $process) {
+    echo 'hello';
+    $process->exec('/usr/local/php/bin/php',[__DIR__.'/../serve/http_server.php']);
+}, true);
+$pid = $process->start();
+echo 'pid:'.$pid.PHP_EOL;
+$process->wait();
+
+
