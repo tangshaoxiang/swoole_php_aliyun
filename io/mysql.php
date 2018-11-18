3aed601976d8c97562fb7d529e66f0e714addb4b
darian <873246925@qq.com>
2018-11-18 17:49:59 +0800

    t_up

--- io/mysql.php
+++ io/mysql.php
@@ -5,37 +5,6 @@
  * Date: 2018/11/18
  * Time: 17:21
  */
-$db = new swoole_mysql();
-$server = array(
-    'host' => '192.168.56.102',
-    'port' => 3306,
-    'user' => 'test',
-    'password' => 'test',
-    'database' => 'test',
-    'charset' => 'utf8', //指定字符集
-    'timeout' => 2,  // 可选：连接超时时间（非查询超时时间），默认为SW_MYSQL_CONNECT_TIMEOUT（1.0）
-);
-
-$db->connect($server, function ($db, $r) {
-    if ($r === false) {
-        var_dump($db->connect_errno, $db->connect_error);
-        die;
-    }
-    $sql = 'show tables';
-    $db->query($sql, function(swoole_mysql $db, $r) {
-        if ($r === false)
-        {
-            var_dump($db->error, $db->errno);
-        }
-        elseif ($r === true )
-        {
-            var_dump($db->affected_rows, $db->insert_id);
-        }
-        var_dump($r);
-        $db->close();
-    });
-});
-
 class mysql{
     public $mysql = '';
     public $config = '';
@@ -65,8 +34,10 @@ class mysql{
     }
 
     /**
-     * mysql 执行逻辑
+     *  mysql 执行逻辑
      * @param $id
+     * @param $username
+     * @return bool
      */
     public function execute($id,$username){
         $this->mysql->connect($this->config,function ($mysql,$result){
@@ -75,7 +46,8 @@ class mysql{
                 die;
             }
         });
-
+        echo 111;
+return  true;
         $sql = 'select * from test where id = '.$id;
         $this->mysql->query($sql,function ($db,$res){
             //$res   当为select时返回结果集，当为add,del,update时返回bool
