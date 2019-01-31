d82b59bfa9edeaa3f4d3b2c427590b6e32eed19a
darian <873246925@qq.com>
2019-01-31 15:55:41 +0800

    't_up'

--- serve/udp.php
+++ serve/udp.php
@@ -2,10 +2,10 @@
 //创建Server对象，监听 127.0.0.1:9502端口，类型为SWOOLE_SOCK_UDP
 $serv = new swoole_server("0.0.0.0", 9502, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
 
-$serv->set([
-    'worker_num' => 4,    //worker进程数   一般是cpu的倍数
-    'max_request' => 1000      //最大请求数
-]);
+//$serv->set([
+//    'worker_num' => 4,    //worker进程数   一般是cpu的倍数
+//    'max_request' => 1000      //最大请求数
+//]);
 
 //监听数据接收事件
 $serv->on('Packet', function ($serv, $data, $clientInfo) {
