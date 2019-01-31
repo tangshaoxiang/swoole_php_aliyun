3b77ba756204a7012963de73981193cb08b7b489
darian <873246925@qq.com>
2019-01-31 17:05:53 +0800

    't_up'

---
+++ serve/tcp_asy.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ty_Ro
+ * Date: 2019/1/31
+ * Time: 16:58
+ */
+$serv = new swoole_server("0.0.0.0",9501);
+
+$serv->set(array('task_worker_num'=>4));
+
+//投递异步任务
+$serv->on("receive",function ($serv,$fd,$from_id,$data){
+    $tadk_id = $serv->task($data);
+    echo "异步 ID：　$tadk_id\n";
+});
+
+//处理异步任务
+$serv->on('task',function ($serv,$task_id,$from_id,$data){
+   echo "执行 异步ID： $task_id";
+   $serv->finish("$data->OK");
+});
+
+
+//处理结果
+$serv->on('finish',function ($serv,$task_id,$data){
+    echo "执行成功";
+});
+
+$serv->start();
